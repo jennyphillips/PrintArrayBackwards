e60759977091bd0c05e8e855b7ade3fd25f5c619
jenny <jenny.phillips.86@hotmail.com>
2016-12-20 11:33:38 -0800

    Added .gitignore

--- main.cpp
+++ main.cpp
@@ -25,12 +25,19 @@ using namespace std;
 
 
 int main(){
+    cout << "Enter integer for array size in range from 1 to 1000" << endl;
+
     int n = 0;
     cin >> n;
+
     if (n < 1 || n > std::pow(10, 3))
     {
-        throw invalid_argument("invalid size indicated: " + n);
+        throw invalid_argument("Invalid size indicated: " + n);
     }
+
+    cout << "Enter " << n << " integer(s) to the array" << endl
+    << "each in range from 1 to 10000" << endl
+    << "separated by space: ";
 
     vector<int> arr(n);
     for (int arr_i = 0; arr_i < n; arr_i++)
@@ -40,7 +47,7 @@ int main(){
 
         if (a < 1 || a > std::pow(10, 4))
         {
-            throw invalid_argument("invalid integer indicated: " + a);
+            throw invalid_argument("Invalid integer indicated: " + a);
         }
         arr[arr_i] = a;
     }
@@ -48,6 +55,7 @@ int main(){
     cin.clear();
     cin.ignore();
 
+    cout << "Array backwards: ";
     for (int i = arr.size()-1; i >= 0; i--)
     {
         cout << arr[i] << " ";
